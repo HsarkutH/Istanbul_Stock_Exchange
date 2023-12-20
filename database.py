03854ca8f7a0ea16093278fdcdad40f420c81d02
yeetmre <211805041@stu.adu.edu.tr>
2023-12-20 15:06:52 +0300

    database testing with json files

--- database.py
+++ database.py
@@ -1,5 +1,6 @@
 import yfinance as yf
 import sqlite3
+import json
 
 # Connect to SQLite database
 conn = sqlite3.connect('stock_data.db')
@@ -7,21 +8,25 @@ cursor = conn.cursor()
 
 # Create a table to store stock data
 cursor.execute('''CREATE TABLE IF NOT EXISTS stock_data
-                (ticker TEXT, date TEXT, open REAL, high REAL, low REAL, close REAL, volume INTEGER)''')
+                (ticker TEXT, stock_name TEXT, date TEXT, open REAL, high REAL, low REAL, close REAL, volume INTEGER)''')
 
-# List of stock tickers for Istanbul Stock
-stock_tickers = ['ACSEL.IS', 'ADEL.IS', 'ADESE.IS']
+# Read stock tickers and names from JSON file
+with open('stock_data.json', 'r') as json_file:
+    stock_data = json.load(json_file)
 
-# Iterate through each stock ticker
-for ticker in stock_tickers:
+# Fetch and insert stock data into the table
+for ticker, stock_name in stock_data.items():
     # Fetch the stock data using yfinance
     stock = yf.Ticker(ticker)
     data = stock.history(period='1d')
 
-    # Insert data into the table
+    # Insert data into the table, checking for duplicates
     for index, row in data.iterrows():
-        cursor.execute("INSERT INTO stock_data VALUES (?, ?, ?, ?, ?, ?, ?)",
-                       (ticker, index.strftime('%Y-%m-%d'), row['Open'], row['High'], row['Low'], row['Close'], row['Volume']))
+        cursor.execute("SELECT * FROM stock_data WHERE ticker=? AND date=?", (ticker, index.strftime('%Y-%m-%d')))
+        existing_data = cursor.fetchone()
+        if not existing_data:
+            cursor.execute("INSERT INTO stock_data VALUES (?, ?, ?, ?, ?, ?, ?, ?)",
+                           (ticker, stock_name, index.strftime('%Y-%m-%d'), row['Open'], row['High'], row['Low'], row['Close'], row['Volume']))
 
 # Commit the changes and close the connection
 conn.commit()

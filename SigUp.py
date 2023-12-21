70f8413e2c997f2e2a8598987fb706346e7abf3b
Perviz Guliyev <alekpika111@mail.ru>
2023-12-22 00:49:38 +0300

    Adding Icon Folder

---
+++ SigUp.py
@@ -0,0 +1,80 @@
+from PySide6.QtCore import (QCoreApplication, QDate, QDateTime, QLocale,
+    QMetaObject, QObject, QPoint, QRect,
+    QSize, QTime, QUrl, Qt)
+from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
+    QFont, QFontDatabase, QGradient, QIcon,
+    QImage, QKeySequence, QLinearGradient, QPainter,
+    QPalette, QPixmap, QRadialGradient, QTransform)
+from PySide6.QtWidgets import (QApplication, QLabel, QLineEdit, QPushButton,
+    QSizePolicy, QWidget)
+#import Icon_r
+
+class Ui_Form(object):
+    def setupUi(self, Form):
+        if not Form.objectName():
+            Form.setObjectName(u"Form")
+        Form.resize(277, 277)
+        Form.setMaximumSize(QSize(482, 390))
+        icon = QIcon()
+        icon.addFile(u":/icon/icon.png", QSize(), QIcon.Normal, QIcon.Off)
+        Form.setWindowIcon(icon)
+        Form.setStyleSheet(u"QWidget { background-color :#d1d5db;\n"
+"}\n"
+"QPushButton { background-color: rgb(107, 114, 128);\n"
+"color: rgb(255, 255, 255)\n"
+"}\n"
+"QLineEdit {background-color: #9ca3af\n"
+"}\n"
+"QLineEdit{ color: rgb(0, 0, 0);\n"
+"border: 0px;\n"
+"border-radius:10px\n"
+"}")
+        self.pushButton = QPushButton(Form)
+        self.pushButton.setObjectName(u"pushButton")
+        self.pushButton.setGeometry(QRect(60, 210, 160, 31))
+        self.lineEdit = QLineEdit(Form)
+        self.lineEdit.setObjectName(u"lineEdit")
+        self.lineEdit.setGeometry(QRect(10, 30, 261, 31))
+        self.lineEdit_2 = QLineEdit(Form)
+        self.lineEdit_2.setObjectName(u"lineEdit_2")
+        self.lineEdit_2.setGeometry(QRect(10, 90, 261, 31))
+        self.lineEdit_2.setEchoMode(QLineEdit.PasswordEchoOnEdit)
+        self.lineEdit_3 = QLineEdit(Form)
+        self.lineEdit_3.setObjectName(u"lineEdit_3")
+        self.lineEdit_3.setGeometry(QRect(10, 150, 261, 31))
+        self.lineEdit_3.setFocusPolicy(Qt.StrongFocus)
+        self.lineEdit_3.setEchoMode(QLineEdit.PasswordEchoOnEdit)
+        self.label = QLabel(Form)
+        self.label.setObjectName(u"label")
+        self.label.setGeometry(QRect(10, 10, 61, 16))
+        self.label_2 = QLabel(Form)
+        self.label_2.setObjectName(u"label_2")
+        self.label_2.setGeometry(QRect(10, 70, 81, 16))
+        self.label_3 = QLabel(Form)
+        self.label_3.setObjectName(u"label_3")
+        self.label_3.setGeometry(QRect(10, 130, 101, 16))
+
+        self.retranslateUi(Form)
+
+        QMetaObject.connectSlotsByName(Form)
+    # setupUi
+
+    def retranslateUi(self, Form):
+        Form.setWindowTitle(QCoreApplication.translate("Form", u"Istanbul Exchange Software", None))
+        self.pushButton.setText(QCoreApplication.translate("Form", u"Sign Up", None))
+        self.label.setText(QCoreApplication.translate("Form", u"Username:", None))
+        self.label_2.setText(QCoreApplication.translate("Form", u"Set Password:", None))
+        self.label_3.setText(QCoreApplication.translate("Form", u"Confirm Password:", None))
+    # retranslateUi
+
+
+if __name__ == "__main__":
+        app = QApplication([])
+
+        dialog = QWidget()
+        ui = Ui_Form()
+        ui.setupUi(dialog)
+
+        dialog.show()
+
+        app.exec()
